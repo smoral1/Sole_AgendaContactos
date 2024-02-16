4fe86d6eba87dde75e4ce33340e75d4c21e97c56
sole <soledadmoral@13gmail.com>
2024-02-16 12:27:36 +0100

    docs(contacto): add documentationç

--- src/modelo/javabean/Contacto.java
+++ src/modelo/javabean/Contacto.java
@@ -2,15 +2,25 @@ package modelo.javabean;
 
 import java.util.Objects;
 
-
+/**
+ * Clase que representa un contacto.
+ * @author Sole Moral
+ */
 public class Contacto {
 	private String nombre;
 	private String apellidos;
 	private String telefono;
 	private String email;
 	private String empresa;
-	
-	
+
+	/**
+	 * Constructor entero de la clase Contacto.
+	 * @param nombre Nombre del contacto.
+	 * @param apellidos Apellidos del contacto.
+	 * @param telefono Teléfono del contacto.
+	 * @param email Email del contacto.
+	 * @param empresa Nombre de la empresa donde trabaja el contacto.
+	 */
 	public Contacto(String nombre, String apellidos, String telefono, String email, String empresa) {
 		super();
 		this.nombre = nombre;
@@ -20,10 +30,13 @@ public class Contacto {
 		this.empresa = empresa;
 	}
 
+	/**
+	 * Constructor sin nada de la clase Contacto.
+	 */
 	public Contacto() {
 		super();
 	}
-	
+
 
 	public String getNombre() {
 		return nombre;
@@ -64,13 +77,22 @@ public class Contacto {
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
-	
 
+	/**
+	 *El método hashCode() complementa al método equals,
+	 * sirve para comparar objetos de una forma más rápida en estruturas.
+	 * @return el hashCode del nombre del contacto
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(nombre);
 	}
 
+	/**
+	 * Método para verificar si dos contactos son iguales por el nombre
+	 * @param obj Objeto que se va a comparar.
+	 * @return true si los nombres son iguales, false si no lo son
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -82,6 +104,7 @@ public class Contacto {
 		Contacto other = (Contacto) obj;
 		return Objects.equals(nombre, other.nombre);
 	}
+
 
 	@Override
 	public String toString() {
